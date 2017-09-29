f712e36dbacb610a73d8ba35d115ac9483c5ac31
dannymcwaves <dannymcwaves96@gmail.com>
2017-09-29 20:01:02 +0000

    data models to be used by mongooose for defining an object structure and mongooose methods

---
+++ modules/data/server/models/data.model.js
@@ -0,0 +1,7 @@
+// this module contains the model of the data that is to be
+// created, read, updated or deleted.
+// i am yet to come up with a model like that.
+
+
+let path = require('path'),
+  config = require(path.resolve('./config/config'));
